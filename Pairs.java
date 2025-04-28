c9ff2ca486d147064f3220e5ca29af1cfd8d99cb
niru0744s <nirupambhattacharya100@gmail.com>
2025-04-29 01:51:40 +0530

    second

--- Pairs.java
+++ Pairs.java
@@ -1,5 +1,17 @@
 public class Pairs {
+    public static void Pair(int n[]){
+        int curr = 0;
+        for(int i = 0; i<n.length; i++){
+            curr = n[i];
+            for(int j=i+1; j<n.length; j++){
+                System.out.print("("+curr+","+n[j]+")");
+            }
+            System.out.println();
+        }
+        System.out.println("total num of pairs = "+ (n.length*(n.length-1))/2);
+    }
     public static void main(String argc[]){
-        
+        int arr[] = {2,4,6,8,10};
+        Pair(arr);
     }
 }
